1f6c6944b3f20f016cba4b07cb0c6292db8bade0
node <ratafak15@gmail.com>
2024-03-07 12:19:41 -0500

    Bomberman mapa termiando introduciendo el personaje

--- src/game/entities/BattleHud.js
+++ src/game/entities/BattleHud.js
@@ -1,4 +1,5 @@
 import { Entity } from 'engine/Entity.js';
+import { SCREEN_WIDTH, STAGE_OFFSET_Y } from 'game/constants/game.js';
 
 export class BattleHud extends Entity {
   constructor(position) {
@@ -12,6 +13,6 @@ export class BattleHud extends Entity {
   }
 
   draw(context, camera) {
-    // Add your main draw calls here
+    context.drawImage(this.image, 8, 40, SCREEN_WIDTH, STAGE_OFFSET_Y, 0, 0, SCREEN_WIDTH, STAGE_OFFSET_Y);
   }
 }
